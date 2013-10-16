36076d4037a84856eb93f7c8762bab44bca34fea
Doug Miller <dmiller@iseatz.com>
2013-10-16 13:58:58 -0500

    left justifying dir text

--- .git-ls-branch.sh
+++ .git-ls-branch.sh
@@ -12,10 +12,10 @@ __git_ls_branch () {
     then
       cd $file
       ps1=`__git_ps1`
-      printf "%30s %15s\n" "$file" "$ps1"
+      printf "%-30s %15s\n" "$file" "$ps1"
       cd ..
     else
-      printf "%30s\n" "$file"
+      printf "%-30s\n" "$file"
     fi
   done
 
