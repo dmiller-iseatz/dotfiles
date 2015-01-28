c2e940b65ce80b4684ee4c9ab4d784869dc8d90d
Doug Miller <dmiller@iseatz.com>
2015-01-28 10:44:55 -0600

    Moving .vim dir

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ function __dm_link_up() {
   ln -sf $HOME/dotfiles/links/.gitignore $HOME/
   ln -sf $HOME/dotfiles/links/.gitconfig $HOME/
   ln -sf $HOME/dotfiles/links/.vimrc $HOME/
+  ln -sf $HOME/dotfiles/links/.vim $HOME/
   ln -sf $HOME/dotfiles/links/.git-prompt.sh $HOME/
   ln -sf $HOME/dotfiles/links/git-completion.bash $HOME/
 }
