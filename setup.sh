e9808258a27bd1f27ae91bdc8774172270caf362
Doug Miller <dmiller@iseatz.com>
2015-01-28 10:28:21 -0600

    fixing setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 function __dm_link_up() {
-  ln -sf $HOME/links/.dougrc $HOME/
-  ln -sf $HOME/links/.gitignore $HOME/
-  ln -sf $HOME/links/.gitconfig $HOME/
-  ln -sf $HOME/links/.vimrc $HOME/
-  ln -sf $HOME/links/.git-prompt.sh $HOME/
-  ln -sf $HOME/links/git-completion.bash $HOME/
+  ln -sf $HOME/dotfiles/links/.dougrc $HOME/
+  ln -sf $HOME/dotfiles/links/.gitignore $HOME/
+  ln -sf $HOME/dotfiles/links/.gitconfig $HOME/
+  ln -sf $HOME/dotfiles/links/.vimrc $HOME/
+  ln -sf $HOME/dotfiles/links/.git-prompt.sh $HOME/
+  ln -sf $HOME/dotfiles/links/git-completion.bash $HOME/
 }
 
-__dm_link_up()
+__dm_link_up
 
