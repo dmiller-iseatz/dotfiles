a63f5aebfba3e993733cb14168cd0babfc07a9b6
Doug Miller <dmiller@iseatz.com>
2016-04-18 16:14:27 -0500

    Updating ls branch

--- links/.git-ls-branch.sh
+++ links/.git-ls-branch.sh
@@ -6,17 +6,15 @@
 
 __git_ls_branch () {
 
-  for item in `ls`
+  for item in */
   do
     if [ -d $item ] && [ -d $item/.git ]
     then
       cd $item
       ps1=`__git_ps1`
-      printf "%-40s %25s\n" "$item" "$ps1"
+      printf "%s %s\n" "$item" "$ps1"
       cd ..
-    else
-      printf "%-30s\n" "$item"
     fi
-  done
+  done | column -t
 
 }
